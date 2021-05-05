52fc517c42b2b518558811c92f6c552fc6d0f2d5
Patrick Dunn <pmdunn78@gmail.com>
2021-05-05 09:11:04 -0500

    update 'view emps' w/manager name

--- db/index.js
+++ db/index.js
@@ -15,10 +15,16 @@ const viewRoles = () => {
 
 const viewEmployees = () => {
     return connection.query(`
-        SELECT employee.id, employee.first_name, employee.last_name, role.title, departments.department 
-        FROM employee 
+        SELECT e1.id AS ID, 
+        CONCAT(e1.first_name, " ", e1.last_name) AS Employee, 
+        role.title AS Title,
+        departments.department AS Department,
+        CONCAT(e2.first_name, " ", e2.last_name) AS Manager
+        FROM employee e1
+        LEFT JOIN employee e2
+        ON e1.manager_id = e2.id
         LEFT JOIN role 
-        ON employee.role_id = role.id
+        ON e1.role_id = role.id
         LEFT JOIN departments 
         ON role.departments_id = departments.id;`
     );
